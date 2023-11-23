3e4c20653670bbc3a69d80fce9d6b8e496449606
Jeffrey Chen <jeffreychen0706@gmail.com>
2023-11-23 16:30:11 +0800

    Guard page refactor

---
+++ frontend/src/ui/Component/ParkingLot.jsx
@@ -0,0 +1,62 @@
+import './ParkingLot.css';
+import {
+  Container,
+  Row,
+  Col
+} from 'reactstrap';
+import React, { useState } from 'react';
+import ViewLots, { LOT_STATUS } from '../Component/ViewLots';
+import RoadLineVertical from '../../assets/RoadLineVertical.svg';
+import RoadLineHorizontal from '../../assets/RoadLineHorizontal.svg';
+
+
+function ParkingLot({ layout=[['A', 'B'], ['C', 'D']] }) {
+
+  if (layout.length === 0) {
+    throw new Error('Layout error, it should be a 2D array');
+  }
+  else {
+    for (let i = 0; i < layout.length; i++) {
+      if (!Array.isArray(layout[i])) {
+        throw new Error('Layout error, it should be a 2D array');
+      }
+    }
+  }
+
+  const render_lots = (sect) => {
+    let array = [];
+    for (let i = 0; i < 6; i++) {
+      let randomNumber = Math.floor(Math.random() * Object.keys(LOT_STATUS).length); // Generates a random number between 0 and 3
+      array.push(randomNumber);
+    }
+    return (
+      <div className="wrap-lots-container-1x">
+        <ViewLots SECTION={sect} LOTs_STATUS={array} />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <img className='RoadLineVertical' src={RoadLineVertical} alt="" />
+      <img className='RoadLineHorizontal' src={RoadLineHorizontal} alt="" />
+      <Container className="wrap-lots-container-4x">
+        {
+          layout.map((row, rowIndex) => (
+            <Row className="wrap-lots-container-2x" key={rowIndex}>
+              {
+                row.map((col, colIndex) => (
+                  <Col key={colIndex}>
+                    {render_lots(col)}
+                  </Col>
+                ))
+              }
+            </Row>
+          ))
+        }
+      </Container>
+    </div>
+  );
+}
+
+export default ParkingLot;
--- frontend/src/ui/Page/GuardMonitor.jsx
+++ frontend/src/ui/Page/GuardMonitor.jsx
@@ -1,70 +1,25 @@
 import './GuardMonitor.css';
-import { Button } from 'reactstrap';
 import React, { useState } from 'react';
 import Header, { TOGGLER_TYPE } from '../Component/Header';
 import SubHeader, { INFO_TYPE } from '../Component/SubHeader';
-import ViewLots, { CARS_STATUS } from '../Component/ViewLots';
-import RoadLineVertical from '../../assets/RoadLineVertical.svg';
-import RoadLineHorizontal from '../../assets/RoadLineHorizontal.svg';
+import ParkingLot from '../Component/ParkingLot';
+import Floors from '../Component/Floors';
 
 
 function GuardMonitor() {
 
   const floors = ['1F', '2F', '3F', '4F'];
-  const [clicked_floor, setFloor] = useState('1F');
-  const togglerClicked = (event) => {
-    setFloor(event.target.textContent);
-  }
-
-  const render_lots = (sect) => {
-    let array = [];
-    for (let i = 0; i < 6; i++) {
-      let randomNumber = Math.floor(Math.random() * 4); // Generates a random number between 0 and 3
-      array.push(randomNumber);
-    }
-    return (
-      <div className="wrap-lots-container-1x">
-        <ViewLots SECTION={sect} LOTs_STATUS={array} />
-        {/* <img className='AutosImg' src={autos} alt="" /> */}
-      </div>
-    )
+  const [currentFloor, setCurrentFloor] = useState(floors[0]);
+  const onClick = (event) => {
+    setCurrentFloor(event.target.innerText);
   }
 
   return (
     <>
       <Header togglerType={TOGGLER_TYPE.EXIT} />
       <SubHeader BACK_ICON={true} LEFT_STR="Parking Slot 1" RHS_INFO={INFO_TYPE.NONE} />
-
-      <img className='RoadLineVertical' src={RoadLineVertical} alt="" />
-      <img className='RoadLineHorizontal' src={RoadLineHorizontal} alt="" />
-
-      <div className="wrap-lots-container-4x">
-        <div className="wrap-lots-container-2x">
-          {render_lots('D')}
-          {render_lots('C')}
-        </div>
-
-        <div className="wrap-lots-container-2x">
-          {render_lots('B')}
-          {render_lots('A')}
-        </div>
-      </div>
-
-
-      <div className="floors-container">
-        {floors.map((floor, index) => (
-          <Button
-            key={index}
-            outline={clicked_floor !== floor}
-            color='dark'
-            className={`floor ${clicked_floor === floor ? 'clicked_floor' : ''}`}
-            onClick={(e) => togglerClicked(e)}
-          >
-            {floor}
-          </Button>
-        ))}
-      </div >
-
+      <ParkingLot />
+      <Floors floors={floors} currentFloor={currentFloor} onClick={onClick}/>
     </>
   );
 }
