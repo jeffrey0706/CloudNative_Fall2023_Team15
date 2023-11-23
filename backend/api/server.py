a62a3a1fcac4344ba63251835d62d5c9f4e44411
ander1119 <b07902060@ntu.edu.tw>
2023-11-23 15:09:23 +0800

    fix conflict part in server.py

--- backend/api/server.py
+++ backend/api/server.py
@@ -2,13 +2,13 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 
-from database import *
+from database.models import *
 
 import datatime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:iamgroot@localhost:3307/test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
