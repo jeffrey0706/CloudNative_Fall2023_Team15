b87151f5c4e2b11f2b8d1c001ea37f546087b4ea
Dobby-the-elf <jeff605136051398@gmail.com>
2023-11-23 23:55:47 +0800

    Add section image on `ReservePage'

--- frontend/src/ui/Component/ViewLotsSet.jsx
+++ frontend/src/ui/Component/ViewLotsSet.jsx
@@ -1,14 +1,32 @@
 import './ViewLotsSet.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ViewLots from './ViewLots';
 import RoadLineDirection from '../../assets/RoadLineDirection.svg'
 
 function ViewLotsSet({ SECTION = 'A', LOTs_STATUS = [2, 2, 3, 2, 1, 1] }) {
+
+    const [sectionImg, setSectionImg] = useState(null);
+
+    useEffect(() => {
+        async function loadSectionImage() {
+            try {
+                const sectionImgPath = await import(`../../assets/section${SECTION}.svg`);
+                setSectionImg(sectionImgPath.default); // Assuming default export
+            } catch (error) {
+                console.error("Import failed", error);
+            }
+        }
+        loadSectionImage();
+    }, [SECTION]);
+
     return (
         <div className="wrap-container">
             <div className="wrap-lots-container">
                 <div className="wrap-dir-container">
-                    <div className="temp-container">Temp.</div>
+                    <div className="temp-container">
+                        {/* Temp. */}
+                        {sectionImg && <img className='SectionImg' src={sectionImg} alt={`${SECTION}`} />}
+                    </div>
                     <img className='RoadLineDirection' src={RoadLineDirection} alt="" />
                 </div>
 
