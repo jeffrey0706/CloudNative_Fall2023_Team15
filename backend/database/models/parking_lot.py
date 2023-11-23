408ed3e348becac6fa7e688b2dd010923d31010c
ander1119 <b07902060@ntu.edu.tw>
2023-11-23 15:33:25 +0800

    rename parkinglot row name

--- backend/database/models/parking_lot.py
+++ backend/database/models/parking_lot.py
@@ -1,6 +1,6 @@
 from api import db
 
 class ParkingLot(db.Model):
-    parking_lot = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    parking_lot_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(255))
     spot_counts = db.Column(db.Integer)
