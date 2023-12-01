1d6f72f33a943777862f5d563f0fd5f82e678632
Dobby-the-elf <jeff605136051398@gmail.com>
2023-12-01 18:20:03 +0800

    Bug fix for `ProgressBar` (prevent infinite loop)

--- frontend/src/test/Component/ProgressBar.test.jsx
+++ frontend/src/test/Component/ProgressBar.test.jsx
@@ -9,10 +9,6 @@ jest.mock('react-circular-progressbar', () => ({
 }));
 
 describe('ProgressBar Component', () => {
-    // const testLocations = [
-    //     { remain: 50, capacity: 100 },
-    //     { remain: 30, capacity: 50 }
-    // ];
     const testLocations = [
         {
             name: 'Parking Lot 2',
@@ -26,18 +22,20 @@ describe('ProgressBar Component', () => {
             capacity: 40,
         }
     ]
-    // test('renders with default props', () => {
-    //     render(<ProgressBar />);
-    //     expect(screen.getByText('0 (0%)')).toBeInTheDocument();
-    //     expect(screen.getByText('out of 0 parking spots used')).toBeInTheDocument();
-    // });
+    test('renders with default props', async () => {
+        render(<ProgressBar />);
+        await waitFor(() => {
+            expect(screen.getByText('100 (100%)')).toBeInTheDocument();
+            expect(screen.getByText('out of 100 parking spots used')).toBeInTheDocument();
+        }, { timeout: 2000 });
+    });
 
     test('renders correctly with locations prop', async () => {
         render(<ProgressBar locations={testLocations} />);
         await waitFor(() => {
-            expect(screen.getByText('66 (60%)')).toBeInTheDocument();
+            expect(screen.getByText('44 (40%)')).toBeInTheDocument();
             expect(screen.getByText('out of 110 parking spots used')).toBeInTheDocument();
-        }, { timeout: 3000 });
+        }, { timeout: 2000 });
     });
 
 });
