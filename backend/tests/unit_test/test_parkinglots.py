af2d495706c49f4ffb4043fe660df08534cda6d8
ander1119 <b07902060@ntu.edu.tw>
2023-12-12 20:42:14 +0800

    modified mock object

--- backend/tests/unit_test/test_parkinglots.py
+++ backend/tests/unit_test/test_parkinglots.py
@@ -15,23 +15,39 @@ class CheckParkingLotAPI(UnitTestSettingBase):
                              mock_parkingspot,
                              mock_area,
                              mock_attendance):
-        mock_parkinglot.query.all.return_value = [ParkingLot(ParkingLotID=1, Name='Test', SpotCounts=10)]
+        parking_spots = [
+            ParkingSpot(ParkingSpotID=1, AreaID=1, Priority='Normal'), 
+            ParkingSpot(ParkingSpotID=2, AreaID=1, Priority='Normal'), 
+            ParkingSpot(ParkingSpotID=3, AreaID=1, Priority='Handicap'), 
+            ParkingSpot(ParkingSpotID=4, AreaID=1, Priority='Handicap')
+        ]
+
+        mock_parkinglot.query.all.return_value = [ParkingLot(ParkingLotID=1, Name='Test', SpotCounts=4)]
+        mock_parkingspot.query.all.return_value = parking_spots
         mock_reservation.query.return_value.all.return_value = [Reservation(ParkingSpotID=1)]
-        mock_parkingspot.query.filter.return_value.all.return_value = [ParkingSpot(AreaID=1)]
-        # use side effect to assign two different return value for each call
-        mock_area.query.filter.return_value.all.side_effect = [[Area(ParkingLotID=1)], [Area(ParkingLotID=1), Area(ParkingLotID=1)]]
         mock_attendance.query.return_value.all.return_value = [Attendance(ParkingSpotID=1)]
+        mock_parkingspot.query.filter.return_value.all.side_effect = [
+            [parking_spots[0]],
+            [parking_spots[1], parking_spots[2]]
+        ]
+        # use side effect to assign two different return value for each call
+        mock_area.query.filter.return_value.all.side_effect = [
+            [Area(ParkingLotID=1), Area(ParkingLotID=1), Area(ParkingLotID=1), Area(ParkingLotID=1)],
+            [Area(ParkingLotID=1)], 
+            [Area(ParkingLotID=1), Area(ParkingLotID=1)]
+        ]
 
         response = self.client.get('/parkinglots')
         self.assert200(response)
         result = response.get_json()
-
         expected_result = [
             {
                 'parkinglot_id': 1,
                 'name': 'Test',
-                'current_capacity': 7,
-                'maximum_capacity': 10,
+                'current_capacity': 1,
+                'maximum_capacity': 4,
+                'current_handicap_capacity': 1,
+                'maximum_handicap_capacity': 2,
             }
         ]
 
