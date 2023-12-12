92b4016a4c6150e3d78cdda2789e9f995eae5980
ander1119 <b07902060@ntu.edu.tw>
2023-12-12 20:41:56 +0800

    add handicap capacity in response

--- backend/app/api/parkinglot_api.py
+++ backend/app/api/parkinglot_api.py
@@ -1,3 +1,4 @@
+import copy
 from flask import Blueprint, jsonify
 from typing import List
 
@@ -17,41 +18,55 @@ def parking_lots():
                 name: string,
                 current_capacity: int,
                 maximum_capacity: int,
+                current_handicap_capacity: int,
+                maximum_handicap_capacity: int,
             }
         ]
 
     TODO handicap, coordinate and address should be add into database
     '''
     all_parking_lots: List[ParkingLot] = ParkingLot.query.all()
-    current_capacity = {p.ParkingLotID: 0 for p in all_parking_lots}
+    current_capacity = {p.ParkingLotID: p.SpotCounts for p in all_parking_lots}
+
+    parking_spots: List[ParkingSpot] = ParkingSpot.query.all()
+    areas: List[Area] = Area.query.filter(Area.AreaID.in_([ps.AreaID for ps in parking_spots])).all()
+    maximum_handicap_capacity = {p.ParkingLotID: 0 for p in all_parking_lots}
+    for parking_spot, area in zip(parking_spots, areas):
+        if parking_spot.Priority != 'Normal':
+            maximum_handicap_capacity[area.ParkingLotID] += 1
+    current_handicap_capacity = copy.deepcopy(maximum_handicap_capacity)
     
     # query current reservation and compute current capacity
     reservations: List[Reservation] = Reservation.query.all()
     parking_spot_ids = [r.ParkingSpotID for r in reservations]
-    parking_spot: List[parking_spot] = ParkingSpot.query.filter(ParkingSpot.ParkingSpotID.in_(parking_spot_ids)).all()
-    area_ids = [p.AreaID for p in parking_spot]
+    parking_spots: List[parking_spot] = ParkingSpot.query.filter(ParkingSpot.ParkingSpotID.in_(parking_spot_ids)).all()
+    area_ids = [p.AreaID for p in parking_spots]
     areas: List[Area] = Area.query.filter(Area.AreaID.in_(area_ids)).all()
-    parking_lot_ids = [a.ParkingLotID for a in areas]
-    for ids in parking_lot_ids:
-        current_capacity[ids] += 1
+    for parking_spot, area in zip(parking_spots, areas):
+        current_capacity[area.ParkingLotID] -= 1
+        if parking_spot.Priority != 'Normal':
+            current_handicap_capacity[area.ParkingLotID] -= 1
 
     # query current attendance and compute current capacity
     attendances: List[Attendance] = Attendance.query.all()
     parking_spot_ids = [r.ParkingSpotID for r in attendances]
-    parking_spot: List[parking_spot] = ParkingSpot.query.filter(ParkingSpot.ParkingSpotID.in_(parking_spot_ids)).all()
-    area_ids = [p.AreaID for p in parking_spot]
+    parking_spots: List[ParkingSpot] = ParkingSpot.query.filter(ParkingSpot.ParkingSpotID.in_(parking_spot_ids)).all()
+    area_ids = [p.AreaID for p in parking_spots]
     areas: List[Area] = Area.query.filter(Area.AreaID.in_(area_ids)).all()
-    parking_lot_ids = [a.ParkingLotID for a in areas]
-    for ids in parking_lot_ids:
-        current_capacity[ids] += 1
+    for parking_spot, area in zip(parking_spots, areas):
+        current_capacity[area.ParkingLotID] -= 1
+        if parking_spot.Priority != 'Normal':
+            current_handicap_capacity[area.ParkingLotID] -= 1
 
     results = []
     for parking_lot in all_parking_lots:
         status = {
             'parkinglot_id': parking_lot.ParkingLotID,
             'name': parking_lot.Name,
-            'current_capacity': parking_lot.SpotCounts - current_capacity[parking_lot.ParkingLotID],
+            'current_capacity': current_capacity[parking_lot.ParkingLotID],
             'maximum_capacity': parking_lot.SpotCounts,
+            'current_handicap_capacity': current_handicap_capacity[parking_lot.ParkingLotID],
+            'maximum_handicap_capacity': maximum_handicap_capacity[parking_lot.ParkingLotID]
         }
         results.append(status)
 
