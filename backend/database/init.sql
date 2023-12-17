ae8fccb162d67aebdf35918d3063626169daf28c
pinya52 <a30515@gmail.com>
2023-12-17 10:46:17 +0800

    Update AtferDeleteFromAttendances

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -63,8 +63,6 @@ CREATE TABLE Reservations (
     ParkingSpotID int NOT NULL,
     ReservationTime DATETIME,
     ExpiredTime DATETIME,
-    ParkTime DATETIME,
-    ExitTime DATETIME,
     FOREIGN KEY (CarID) REFERENCES Cars(CarID),
     FOREIGN KEY (ParkingSpotID) REFERENCES ParkingSpots(ParkingSpotID),
     CONSTRAINT PK_Reservation PRIMARY KEY (CarID, ParkingSpotID)
@@ -97,8 +95,7 @@ CREATE EVENT IF NOT EXISTS DeleteExpiredReservationsEvent
 ON SCHEDULE EVERY 30 SECOND
 DO
     DELETE FROM Reservations
-    WHERE (DATE_ADD(ExpiredTime, INTERVAL 2 HOUR) <= NOW());
-<<<<<<< HEAD
+    WHERE DATE_ADD(ExpiredTime, INTERVAL 2 HOUR) <= NOW();
 
 CREATE TRIGGER IF NOT EXISTS AtferDeleteFromReservations
 AFTER DELETE ON Reservations
@@ -106,37 +103,31 @@ FOR EACH ROW
     INSERT INTO Records (CarID, ParkingSpotID, ReservationTime, ExpiredTime, ParkTime, ExitTime)
     VALUES (OLD.CarID, OLD.ParkingSpotID, OLD.ReservationTime, OLD.ExpiredTime, NULL, NULL);
 
-CREATE TRIGGER IF NOT EXISTS AtferDeleteFromAttendances
+CREATE TRIGGER IF NOT EXISTS  AtferDeleteFromAttendances
 AFTER DELETE ON Attendances
 FOR EACH ROW
-    UPDATE Records
-    SET ParkTime = (
-        OLD.ParkTime
-    ),
-    ExitTime = (
-        OLD.ExitTime
-    )
-    WHERE ((CarID=OLD.CarID) AND (ParkingSpotID=OLD.ParkingSpotID))
-    ORDER BY RecordID DESC LIMIT 1;
+BEGIN
+    DECLARE recordCount INT;
 
+	SELECT COUNT(*) INTO recordCount
+	FROM Records
+	WHERE CarID = OLD.CarID
+        AND ParkingSpotID = OLD.ParkingSpotID
+        AND ReservationTime <= OLD.ParkTime
+        AND ExpiredTime >= OLD.ParkTime;
 
-    CREATE TRIGGER IF NOT EXISTS AtferDeleteFromAttendances
-    AFTER DELETE ON Attendances
-    FOR EACH ROW
-        UPDATE Records
-        SET ParkTime = (
-            OLD.ParkTime
-        ),
-        ExitTime = (
-            OLD.ExitTime
-        )
-        WHERE ((CarID=OLD.CarID) AND (ParkingSpotID=OLD.ParkingSpotID))
-        ORDER BY RecordID DESC LIMIT 1;
-=======
-
-CREATE TRIGGER IF NOT EXISTS AtferDeleteFromReservations
-AFTER DELETE ON Reservations
-FOR EACH ROW
-INSERT INTO Records (CarID, ParkingSpotID, ReservationTime, ExpiredTime)
-VALUES (OLD.CarID, OLD.ParkingSpotID, OLD.ReservationTime, OLD.ExpiredTime);
->>>>>>> a1bf260d605808528216cc068e26c76cf30be7d0
+	IF recordCount > 0 THEN
+		UPDATE Records
+		SET ParkTime = OLD.ParkTime,
+			ExitTime = OLD.ExitTime
+		WHERE CarID = OLD.CarID
+		    AND ParkingSpotID = OLD.ParkingSpotID
+		    AND ReservationTime <= OLD.ParkTime
+            AND ExpiredTime >= OLD.ParkTime;
+		ORDER BY RecordID DESC
+		LIMIT 1;
+	ELSE
+		INSERT INTO Records (CarID, ParkingSpotID, ReservationTime, ExpiredTime, ParkTime, ExitTime)
+		VALUES (OLD.CarID, OLD.ParkingSpotID, NULL, NULL, OLD.ParkTime, OLD.ExitTime);
+	END IF;
+END;
