e8a8028142d11acd0cb340a796cfca6229aeee22
ander1119 <b07902060@ntu.edu.tw>
2023-12-18 19:44:34 +0800

    add property for parkinglotid

--- backend/app/models/reservation.py
+++ backend/app/models/reservation.py
@@ -4,13 +4,9 @@ class Reservation(db.Model):
     __tablename__ = 'Reservations'
     
     CarID = db.Column(db.Integer, db.ForeignKey('Cars.CarID'), nullable=False, primary_key=True)
-    ParkingSpotID = db.Column(db.Integer, db.ForeignKey('ParkingSpots.ParkingSpotID'), nullable=False, primary_key=True)
+    ParkingSpotID = db.Column(db.Integer, db.ForeignKey('ParkingSpots.ParkingSpotID'), nullable=False, unique=True, primary_key=True)
     ReservationTime = db.Column(db.DateTime)
     ExpiredTime = db.Column(db.DateTime)
-
-    # __table_args__ = (
-    #     db.PrimaryKeyConstraint(['CarID'], ['ParkingSpotID'])
-    # )
 
     def __init__(self, **kwargs):
         super(Reservation, self).__init__(**kwargs)
